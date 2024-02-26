c81ec18c606e086ac731f36fa59dbbe52b6e7932
Corban Procuniar <103233091+corbanpro@users.noreply.github.com>
2024-02-25 18:19:52 -0700

    little bit of styling. needs icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 function App() {
   return (
-    <div>
-      <a href="https://intercontinentle.corbanpro.net">Intercontinentle</a>
-      <a href="https://www.strategyUnlocked.com">Strategy Unlocked</a>
+    <div style={styles.page}>
+      <h1>Portfolio</h1>
+      <a target="_blank" rel="noreferrer" href="https://intercontinentle.corbanpro.net">
+        Intercontinentle
+      </a>
+      <a target="_blank" rel="noreferrer" href="https://www.strategyUnlocked.com">
+        Strategy Unlocked
+      </a>
     </div>
   );
 }
+
+const styles = {
+  page: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    width: "100vw",
+    backgroundColor: "black",
+    color: "white",
+  },
+} as const;
 
 export default App;
